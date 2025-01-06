a2d9954f7aa6c59c3c3cea8651bbca5089a6c5ff
Jason Fevang <jasonfevang@gmail.com>
2025-01-05 23:45:00 -0800

    Superloop, check switch, send different frame

--- main/DmxTest.c
+++ main/DmxTest.c
@@ -1,22 +1,9 @@
-/*
-
-  ESP-IDF DMX Write
-
-  Writes data to the DMX bus. The value of every byte in the DMX packet (except
-  the start code) is incremented by 1 every 1 second.
-
-  Note: this example is for use with the ESP-IDF. It will not work on Arduino!
-
-  Created 20 June 2022
-  By Mitch Weisbrod
-
-  https://github.com/someweisguy/esp_dmx
-
-*/
 #include "dmx/include/driver.h"
 #include "esp_dmx.h"
 #include "esp_log.h"
+#include "freertos/projdefs.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 #include <string.h>
 
 #define TAG "Main"
@@ -25,52 +12,93 @@
 #define RX_PIN 18 // the pin we are using to RX with
 #define EN_PIN 26 // the pin we are using to enable TX on the DMX transceiver
 
-static uint8_t data[DMX_PACKET_SIZE] = {}; // Buffer to store DMX data
+#define SWITCH_PIN 12 // the pin we are using to detect the light switch to control dmx
+
+
+static uint8_t data_on[DMX_PACKET_SIZE] = {}; // Buffer to store DMX data
+static const uint8_t data_off[DMX_PACKET_SIZE] = {}; // Buffer to store DMX data
 
 typedef struct {
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
-  uint8_t white;
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint8_t white;
 } bar_footprint_t;
 
 typedef struct {
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
-  uint8_t white;
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint8_t white;
 } bed_footprint_t;
 
 static const uint16_t BAR_ADDRESS = 1;
 static const uint16_t BED_ADDRESS = BAR_ADDRESS + sizeof(bar_footprint_t);
 
 void app_main() {
-  ESP_LOGI(TAG, "Hello World!");
+    // Initialize dmx
+    const dmx_port_t dmx_num = DMX_NUM_1;
+    dmx_config_t config = DMX_CONFIG_DEFAULT;
+    const int personality_count = 0;
+    dmx_driver_install(dmx_num, &config, NULL, personality_count);
+    dmx_set_pin(dmx_num, TX_PIN, RX_PIN, EN_PIN);
 
-  // Initialize dmx
-  const dmx_port_t dmx_num = DMX_NUM_1;
-  dmx_config_t config = DMX_CONFIG_DEFAULT;
-  const int personality_count = 0;
-  dmx_driver_install(dmx_num, &config, NULL, personality_count);
-  dmx_set_pin(dmx_num, TX_PIN, RX_PIN, EN_PIN);
+    // Initialize switch
+    gpio_set_direction(SWITCH_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(SWITCH_PIN, GPIO_PULLUP_ONLY);
+    bool switch_state = gpio_get_level(SWITCH_PIN);
+    static const bool SWITCH_ON = 1;
+    static const bool SWITCH_OFF = 0;
 
-  // Initialize dmx frame
-  const bar_footprint_t bar_footprint = {0, 0, 0, 0};
-  const bed_footprint_t bed_footprint = {0, 0, 0, 0};
-  memcpy(data + BAR_ADDRESS, &bar_footprint, sizeof(bar_footprint_t));
-  memcpy(data + BED_ADDRESS, &bed_footprint, sizeof(bed_footprint_t));
+    // Initialize dmx fixtures
+    const bar_footprint_t bar_footprint = {100, 0, 0, 20};
+    const bed_footprint_t bed_footprint = {0, 100, 100, 100};
 
-  // Write frame
-  dmx_write(dmx_num, data, DMX_PACKET_SIZE);
+    // Initialize dmx frame
+    memcpy(data_on + BAR_ADDRESS, &bar_footprint, sizeof(bar_footprint_t));
+    memcpy(data_on + BED_ADDRESS, &bed_footprint, sizeof(bed_footprint_t));
+    if(switch_state == SWITCH_ON) {
+        dmx_write(dmx_num, data_on, DMX_PACKET_SIZE);
+    }
+    else{
+        dmx_write(dmx_num, data_off, DMX_PACKET_SIZE);
+    }
 
-  while (1) {
-    TickType_t now = xTaskGetTickCount();
+    const TickType_t DMX_SEND_PERIOD = pdMS_TO_TICKS(30);
+    const TickType_t SWITCH_READ_PERIOD = pdMS_TO_TICKS(50);
+    TickType_t next_dmx_send = 0;
+    TickType_t next_switch_read = 0;
 
-    // Send data and block until it's sent
-    dmx_send_num(dmx_num, DMX_PACKET_SIZE);
-    dmx_wait_sent(dmx_num, DMX_TIMEOUT_TICK);
+    while (1) {
+        TickType_t now = xTaskGetTickCount();
 
-    // Only send a packet every 30ms
-    vTaskDelayUntil(&now, pdMS_TO_TICKS(30));
-  }
+        if(now > next_dmx_send){
+            // Send data and block until it's sent
+            dmx_send_num(dmx_num, DMX_PACKET_SIZE);
+            // dmx_wait_sent(dmx_num, DMX_TIMEOUT_TICK);
+            next_dmx_send = now + DMX_SEND_PERIOD;
+        }
+
+        if(now > next_switch_read){
+
+            // Read switch
+            const bool prev_switch_state = switch_state;
+            switch_state = gpio_get_level(SWITCH_PIN);
+            next_switch_read = now + SWITCH_READ_PERIOD;
+            ESP_LOGI(TAG, "switch %d", switch_state);
+
+            // Switch turned on
+            if(switch_state == SWITCH_ON && prev_switch_state == SWITCH_OFF){
+                // Turn lights on
+                dmx_write(dmx_num, data_on, DMX_PACKET_SIZE);
+            }
+
+            // Switch turned off
+            if(switch_state == SWITCH_OFF && prev_switch_state == SWITCH_ON){
+                // Turn lights off
+                dmx_write(dmx_num, data_off, DMX_PACKET_SIZE);
+            }
+        }
+        vTaskDelay(1);
+    }
 }
