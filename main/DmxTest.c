b2a2ca2863cba2aead946ad2ee9d4e2309a405c6
Jason Fevang <jasonfevang@gmail.com>
2025-01-06 21:48:25 -0800

    Configure clang-format

--- main/DmxTest.c
+++ main/DmxTest.c
@@ -1,9 +1,9 @@
 #include "dmx/include/driver.h"
+#include "driver/gpio.h"
 #include "esp_dmx.h"
 #include "esp_log.h"
 #include "freertos/projdefs.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include <string.h>
 
 #define TAG "Main"
@@ -12,10 +12,10 @@
 #define RX_PIN 18 // the pin we are using to RX with
 #define EN_PIN 26 // the pin we are using to enable TX on the DMX transceiver
 
-#define SWITCH_PIN 12 // the pin we are using to detect the light switch to control dmx
+#define SWITCH_PIN                                                             \
+    12 // the pin we are using to detect the light switch to control dmx
 
-
-static uint8_t data_on[DMX_PACKET_SIZE] = {}; // Buffer to store DMX data
+static uint8_t data_on[DMX_PACKET_SIZE] = {};        // Buffer to store DMX data
 static const uint8_t data_off[DMX_PACKET_SIZE] = {}; // Buffer to store DMX data
 
 typedef struct {
@@ -57,10 +57,9 @@ void app_main() {
     // Initialize dmx frame
     memcpy(data_on + BAR_ADDRESS, &bar_footprint, sizeof(bar_footprint_t));
     memcpy(data_on + BED_ADDRESS, &bed_footprint, sizeof(bed_footprint_t));
-    if(switch_state == SWITCH_ON) {
+    if (switch_state == SWITCH_ON) {
         dmx_write(dmx_num, data_on, DMX_PACKET_SIZE);
-    }
-    else{
+    } else {
         dmx_write(dmx_num, data_off, DMX_PACKET_SIZE);
     }
 
@@ -72,14 +71,14 @@ void app_main() {
     while (1) {
         TickType_t now = xTaskGetTickCount();
 
-        if(now > next_dmx_send){
+        if (now > next_dmx_send) {
             // Send data and block until it's sent
             dmx_send_num(dmx_num, DMX_PACKET_SIZE);
             // dmx_wait_sent(dmx_num, DMX_TIMEOUT_TICK);
             next_dmx_send = now + DMX_SEND_PERIOD;
         }
 
-        if(now > next_switch_read){
+        if (now > next_switch_read) {
 
             // Read switch
             const bool prev_switch_state = switch_state;
@@ -88,13 +87,13 @@ void app_main() {
             ESP_LOGI(TAG, "switch %d", switch_state);
 
             // Switch turned on
-            if(switch_state == SWITCH_ON && prev_switch_state == SWITCH_OFF){
+            if (switch_state == SWITCH_ON && prev_switch_state == SWITCH_OFF) {
                 // Turn lights on
                 dmx_write(dmx_num, data_on, DMX_PACKET_SIZE);
             }
 
             // Switch turned off
-            if(switch_state == SWITCH_OFF && prev_switch_state == SWITCH_ON){
+            if (switch_state == SWITCH_OFF && prev_switch_state == SWITCH_ON) {
                 // Turn lights off
                 dmx_write(dmx_num, data_off, DMX_PACKET_SIZE);
             }
