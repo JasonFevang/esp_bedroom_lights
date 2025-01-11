955be7ad3a1f81de485a046e77e58ce429a2aa58
Jason Fevang <jasonfevang@gmail.com>
2025-01-11 01:35:51 -0800

    wifi: Adapt example code

--- main/main.c
+++ main/main.c
@@ -3,12 +3,24 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "lifx.h"
+#include "nvs_flash.h"
 #include "switch.h"
 #include "wifi.h"
 
 #define TAG "Main"
 
+void init_nvs(){
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
+
 void app_main() {
+    init_nvs();
     wifi_init();
     lifx_init();
     dmx_init();
