726e73dab10114cac0f0dd80479efc902fd5cde2
Jason Fevang <jasonfevang@gmail.com>
2025-01-17 00:50:01 -0800

    lifx: Send and decode serialized packet

---
+++ components/esp_lifx/include/lifx_structs.h
@@ -0,0 +1,36 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <assert.h>
+#include <stdint.h>
+
+#pragma pack(push, 1)
+typedef struct {
+    /* frame */
+    uint16_t size;           // Size of the entire message
+    uint16_t protocol : 12;  // Must be 1024d
+    uint8_t addressable : 1; // Message includes a target address, must be 1
+    uint8_t tagged : 1;      // Determines if Frame Address target field is
+                             // addressing one or all devices
+    uint8_t origin : 2;      // Message origin indicator, must be 0
+    uint32_t source;         // Source identifier. Unique value set by client
+    /* frame address */
+    uint8_t target[8]; // Serial number of device, or 0 for all devices
+    uint8_t reserved[6];
+    uint8_t res_required : 1; // State message required. Always 0
+    uint8_t ack_required : 1;
+    uint8_t : 6;
+    uint8_t sequence; // Wrap around message sequence number
+    /* protocol header */
+    uint64_t : 64;
+    uint16_t type; // Message type, determines payload
+    uint16_t : 16;
+    /* variable length payload follows */
+} lx_protocol_header_t;
+#pragma pack(pop)
+
+// If no pragam pack(1), then size is 40B
+static_assert(sizeof(lx_protocol_header_t) == 36,
+              "Incorrect lx_protocol_header_t size");
+
+#endif // STRUCTS_H
--- main/lifx.c
+++ main/lifx.c
@@ -1,4 +1,5 @@
 #include "lifx.h"
+#include "lifx_structs.h"
 #include "udp.h"
 #include "wifi.h"
 
@@ -18,7 +19,8 @@
 
 static const char *TAG = "lifx";
 
-static char rx_buffer[128];
+static uint8_t rx_buffer[1024];
+static uint8_t tx_buffer[1024];
 static int sock;
 
 void lifx_init() {
@@ -34,6 +36,76 @@ void lifx_init() {
 typedef enum { WAIT_WIFI, TX, RX, DELAY } udp_state_t;
 
 static udp_state_t state;
+
+void print_buffer(const uint8_t *buffer, uint32_t len) {
+    for (uint32_t i = 0; i < len; i++) {
+        printf("%02X ", buffer[i]);
+        if ((i + 1) % 16 == 0) {
+            printf("\n"); // New line every 16 bytes
+        }
+    }
+    printf("\n");
+}
+
+uint32_t populate_tx_buf(uint8_t *buf, uint32_t buf_len) {
+    lx_protocol_header_t header;
+    memset(&header, 0, sizeof(header));
+    header.size = sizeof(lx_protocol_header_t);
+    header.protocol = 1024;
+    header.addressable = 1;
+    header.tagged = 1;
+    header.origin = 0;
+    header.source = 12345678;
+    header.res_required = 0;
+    header.ack_required = 0;
+    header.sequence = 0;
+    header.type = 2;
+    memcpy(buf, &header, sizeof(header));
+    return sizeof(header);
+}
+
+void print_lx_protocol_header(const lx_protocol_header_t *header,
+                              uint32_t buf_len) {
+    printf("lx_protocol_header_t:\n");
+    printf("  Frame:\n");
+    printf("    Size: %u\n", header->size);
+    printf("    Protocol: %u\n", header->protocol);
+    printf("    Addressable: %u\n", header->addressable);
+    printf("    Tagged: %u\n", header->tagged);
+    printf("    Origin: %u\n", header->origin);
+    printf("    Source: %lu\n", header->source);
+
+    printf("  Frame Address:\n");
+    printf("    Target: ");
+    for (int i = 0; i < 8; i++) {
+        printf("%02X", header->target[i]);
+        if (i < 7) {
+            printf(":");
+        }
+    }
+    printf("\n");
+
+    printf("    Res Required: %u\n", header->res_required);
+    printf("    Ack Required: %u\n", header->ack_required);
+    printf("    Sequence: %u\n", header->sequence);
+
+    printf("  Protocol Header:\n");
+    printf("    Type: %u\n", header->type);
+    printf("  Pay load size: %ld\n", buf_len - sizeof(lx_protocol_header_t));
+}
+
+void decode_buf(uint8_t *buf, uint32_t buf_len) {
+    if (buf_len < sizeof(lx_protocol_header_t)) {
+        ESP_LOGW(TAG,
+                 "Received message smaller than lx_protocol_header, %d bytes. "
+                 "ignoring",
+                 sizeof(lx_protocol_header_t));
+        return;
+    }
+    lx_protocol_header_t header;
+    memcpy(&header, buf, sizeof(lx_protocol_header_t));
+    print_lx_protocol_header(&header, buf_len);
+}
 
 /*
 Call from superloop
@@ -56,11 +128,11 @@ void lifx_loop(TickType_t *next_update) {
     }
     case TX: {
         last_send = now;
-        const char *payload = "Message from ESP32";
 
-        const esp_err_t res =
-            my_udp_send(sock, (const uint8_t *)payload, strlen(payload), PORT,
-                        inet_addr(HOST_IP_ADDR));
+        uint32_t data_len = populate_tx_buf(tx_buffer, 1024);
+
+        const esp_err_t res = my_udp_send(sock, tx_buffer, data_len, PORT,
+                                          inet_addr(HOST_IP_ADDR));
         if (res == ESP_FAIL) {
             ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
         } else {
@@ -79,9 +151,8 @@ void lifx_loop(TickType_t *next_update) {
         uint32_t len;
         uint16_t port;
         in_addr_t ip_addr;
-        const esp_err_t res =
-            my_udp_receive(sock, (uint8_t *)rx_buffer, sizeof(rx_buffer) - 1,
-                           &len, &port, &ip_addr);
+        const esp_err_t res = my_udp_receive(
+            sock, rx_buffer, sizeof(rx_buffer) - 1, &len, &port, &ip_addr);
         if (res == ESP_ERR_TIMEOUT) {
             break;
         }
@@ -90,10 +161,9 @@ void lifx_loop(TickType_t *next_update) {
             break;
         }
 
-        rx_buffer[len] = 0; // Null-terminate whatever we received and
-                            // treat like a string
         ESP_LOGI(TAG, "Received %lu bytes from %s:", len, inet_ntoa(ip_addr));
-        ESP_LOGI(TAG, "%s", rx_buffer);
+        print_buffer(rx_buffer, len);
+        decode_buf(rx_buffer, len);
 
         state = DELAY;
         wait_start = now;
